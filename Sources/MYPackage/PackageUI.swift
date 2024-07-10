87ea2c41974b9026dafe8c4451d1ffc47328b57f
Admin <admin@YI1002826LT.local>
2024-07-10 18:27:43 +0530

    package message update

--- Sources/MYPackage/PackageUI.swift
+++ Sources/MYPackage/PackageUI.swift
@@ -12,7 +12,7 @@ public struct PackageUIView: View {
     public init() {}
     @available(iOS 13.0, *)
     public var body: some View {
-        Text("THIS IS THE SWIFTUIVIEW FROM THE PACKAGE")
+        Text("This is the updated swift package")
     }
 }
 
